5b16e4661eca5798dad2387b387e0b32e13265b7
Danil Khisamov <dannillkawow@gmail.com>
2022-02-17 21:47:29 +0300

    parse processed methods on handler init

--- hw05-aop/src/main/java/ru/otus/homework/Log.java
+++ hw05-aop/src/main/java/ru/otus/homework/Log.java
@@ -4,20 +4,24 @@ import java.lang.annotation.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Log {
     class LogInvocationHandler implements InvocationHandler {
         private final Calculable calculable;
+        private final Set<Method> interfaceMethodsToProcess;
 
         public LogInvocationHandler(Calculable calculable) {
             this.calculable = calculable;
+            this.interfaceMethodsToProcess = getInterfaceMethodsToProcess();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (isOverriddenMethodAnnotatedWith(method, Log.class)) {
+            if (interfaceMethodsToProcess.contains(method)) {
                 System.out.printf("executed method: %s, param: %s%n", method.getName(), Arrays.toString(args));
             }
             return method.invoke(calculable, args);
@@ -29,6 +33,21 @@ public @interface Log {
                         .isAnnotationPresent(annotation);
             } catch (NoSuchMethodException e) {
                 return false;
+            }
+        }
+
+        private Set<Method> getInterfaceMethodsToProcess() {
+            return Arrays.stream(calculable.getClass().getDeclaredMethods())
+                    .filter(method -> isOverriddenMethodAnnotatedWith(method, Log.class))
+                    .map(this::getInterfaceMethodFromImpl)
+                    .collect(Collectors.toSet());
+        }
+
+        private Method getInterfaceMethodFromImpl(Method implMethod) {
+            try {
+                return Calculable.class.getDeclaredMethod(implMethod.getName(), implMethod.getParameterTypes());
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
             }
         }
     }
