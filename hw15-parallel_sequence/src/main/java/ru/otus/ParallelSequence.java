c5119b2fae88fba82dbc0cd2dd718519f312721a
Danil Khisamov <dan.khisamov@gmail.com>
2023-03-16 12:10:28 +0300

    fix comments, remove supplier

--- hw15-parallel_sequence/src/main/java/ru/otus/ParallelSequence.java
+++ hw15-parallel_sequence/src/main/java/ru/otus/ParallelSequence.java
@@ -10,29 +10,31 @@ public class ParallelSequence {
     private static final int START = 1;
     private static final int END = 10;
     private static final int NUMS_COUNT = 19;
-    private boolean firstThreadIsActive = true;
+    private int lastActiveThread = 2;
 
     public static void main(String[] args) {
         var ps = new ParallelSequence();
-        new Thread(() -> ps.printSequence(() -> !ps.firstThreadIsActive), "Thread_1").start();
-        new Thread(() -> ps.printSequence(() -> ps.firstThreadIsActive), "Thread_2").start();
+        new Thread(() -> ps.printSequence(1), "Thread_1").start();
+        new Thread(() -> ps.printSequence(2), "Thread_2").start();
     }
 
-    private synchronized void printSequence(Supplier<Boolean> waitCondition) {
+    private synchronized void printSequence(int threadNumber) {
         int delta = 1;
         int num = START;
         int count = 0;
 
         while (!Thread.currentThread().isInterrupted() && count < NUMS_COUNT) {
             try {
-                while (waitCondition.get()) this.wait();
+                while (lastActiveThread == threadNumber) {
+                    this.wait();
+                }
 
                 log.info(String.valueOf(num));
                 count++;
                 num += delta;
                 if (num == START || num == END) delta *= -1;
 
-                firstThreadIsActive = !firstThreadIsActive;
+                lastActiveThread = threadNumber;
                 this.notify();
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
